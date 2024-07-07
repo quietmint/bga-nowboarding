f9f012b17be473c06eb1b7642855b19b982148d1
quietmint <quietmint@users.noreply.github.com>
2024-07-07 10:42:20 -0400

    Reduce ELO gain

--- nowboarding.game.php
+++ nowboarding.game.php
@@ -2416,7 +2416,8 @@ class NowBoarding extends Table
                 'complaint' => $complaint,
             ]);
         } else {
-            $score = $playerCount * $playerCount + 10;
+            // 2p = 14, 3p = 18, 4p = 24, 5p = 30
+            $score = ceil(pow($playerCount, 1.86)) + 10;
             if ($this->getGlobal(N_OPTION_TIMER) != 1) {
                 // Half score with double/unlimited timer
                 $score = ceil($score / 2);
