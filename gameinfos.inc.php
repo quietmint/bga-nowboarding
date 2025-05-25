fea9d7bf54be431340e3531615004b9a8ac287ed
quietmint <quietmint@users.noreply.github.com>
2025-05-25 10:23:27 -0400

    Remove BGA sandbox params

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -103,14 +103,4 @@ $gameinfos = [
         //  minimum possible value: 740
         'max' => null
     ],
-
-    //////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
-
-    // simple : A plays, B plays, C plays, A plays, B plays, ...
-    // circuit : A plays and choose the next player C, C plays and choose the next player D, ...
-    // complex : A+B+C plays and says that the next player is A+B
-    'is_sandbox' => false,
-    'turnControl' => 'simple'
-
-    ////////
 ];
